2a52f57e41eae49fc6b2d986652628125703aeb7
hk-15 <hollyknox15@gmail.com>
2025-04-30 11:35:34 +0100

    Add rules for pawns to take pieces

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -8,31 +8,50 @@ export default class Pawn extends Piece {
     }
 
     getAvailableMoves(board) {
+        function isSquareOccupied(square) {
+            if (board.getPiece(square)) {
+                return true;
+            }
+            return false;
+        }
+
         let location = board.findPiece(this);
+        let availableMoves = [];
         let oneSquareInFront;
         let twoSquaresInFront;
         let startingRow;
         let lastRow;
+        let diagonalLeftAndRightSquares;
+
         if (this.player === Player.WHITE) {
             oneSquareInFront = Square.at(location.row + 1, location.col);
             twoSquaresInFront = Square.at(location.row + 2, location.col);
             startingRow = 1;
             lastRow = 7;
+            diagonalLeftAndRightSquares = new Array (Square.at(location.row + 1, location.col - 1), Square.at(location.row + 1, location.col + 1));
         }
         else {
             oneSquareInFront = Square.at(location.row - 1, location.col);
             twoSquaresInFront = Square.at(location.row - 2, location.col);
             startingRow = 6;
             lastRow = 0;
+            diagonalLeftAndRightSquares = new Array (Square.at(location.row - 1, location.col - 1), Square.at(location.row - 1, location.col + 1));
+
         }
 
-        if(location.row === lastRow || board.getPiece(oneSquareInFront)) {
-            return new Array(0);
+        if (location.row !== lastRow && !isSquareOccupied(oneSquareInFront)) {
+            availableMoves.push(oneSquareInFront);
+            if (location.row === startingRow && !isSquareOccupied(twoSquaresInFront)) {
+                availableMoves.push(twoSquaresInFront);
+            }
+
+            diagonalLeftAndRightSquares.forEach((square) => {
+                if (isSquareOccupied(square) && this.player !== board.getPiece(square).player && board.getPiece(square).constructor.name !== 'King') {
+                    availableMoves.push(square);
+                }
+            })
+
         }
-        if (location.row !== startingRow || board.getPiece(twoSquaresInFront) ) {
-            return new Array(oneSquareInFront);
-        } else {
-            return new Array(oneSquareInFront, twoSquaresInFront);
-        }
+        return availableMoves;
     }
 }
