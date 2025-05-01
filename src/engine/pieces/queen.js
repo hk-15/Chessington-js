d52ea348069cae398b3502e387d3d7fdfb948000
hk-15 <hollyknox15@gmail.com>
2025-05-01 10:34:43 +0100

    Add tests for queen and queen basic moves

--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Square from '../square';
+import GameSettings from '../gameSettings';
 
 export default class Queen extends Piece {
     constructor(player) {
@@ -6,6 +8,69 @@ export default class Queen extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        function isSquareOccupied(square) {
+            if (board.getPiece(square)) {
+                return true;
+            }
+            return false;
+        }
+
+        let location = board.findPiece(this);
+        const boardSize = GameSettings.BOARD_SIZE;
+        const availableMoves = [];
+        //lateral moves 
+        let row = location.row;
+        let col = location.col + 1;
+        while (col < boardSize) {
+            availableMoves.push(Square.at(row, col));
+            col++;
+        }
+        col = location.col - 1;
+        while (col >= 0) {
+            availableMoves.push(Square.at(row, col));
+            col--;
+        }
+        col = location.col;
+        row = location.row + 1;
+        while (row < boardSize) {
+            availableMoves.push(Square.at(row, col));
+            row++;
+        }
+        row = location.row - 1;
+        while (row >= 0) {
+            availableMoves.push(Square.at(row, col));
+            row--;
+        }
+        //diagonal moves
+        row = location.row + 1;
+        col = location.col + 1;
+        while (row < boardSize && col < boardSize) {
+            availableMoves.push(Square.at(row, col));
+            row++;
+            col++;
+        }
+        row = location.row + 1;
+        col = location.col - 1;
+        while (col >= 0 && row < boardSize) {
+            availableMoves.push(Square.at(row, col));
+            row++;
+            col--;
+        }
+        row = location.row - 1;
+        col = location.col + 1;
+        while (row >= 0 && col < boardSize) {
+            availableMoves.push(Square.at(row, col));
+            row--;
+            col++;
+        }
+        row = location.row - 1;
+        col = location.col - 1;
+        while (row >= 0 && col >= 0) {
+            availableMoves.push(Square.at(row, col));
+            row--;
+            col--;
+        }
+        console.log(availableMoves)
+        return availableMoves;
     }
 }
--- tests/engine/pieces/queen.spec.js
+++ tests/engine/pieces/queen.spec.js
@@ -1,5 +1,51 @@
 import Queen from '../../../src/engine/pieces/queen';
+import Board from '../../../src/engine/board';
+import Player from '../../../src/engine/player';
+import Square from '../../../src/engine/square';
 
 describe('Queen', () => {
 
+    let board;
+    beforeEach(() => board = new Board());
+
+    it('can move laterally', () => {
+        const queen = new Queen(Player.WHITE);
+        board.setPiece(Square.at(3, 5), queen);
+
+        const moves = queen.getAvailableMoves(board);
+
+        const expectedMoves = [
+            // Horizontal
+            Square.at(3, 0), Square.at(3, 1), Square.at(3, 2), Square.at(3, 3), Square.at(3, 4), Square.at(3, 6), Square.at(3, 7),
+            // Vertical
+            Square.at(0, 5), Square.at(1, 5), Square.at(2, 5), Square.at(4, 5), Square.at(5, 5), Square.at(6, 5), Square.at(7, 5)
+        ];
+
+        moves.should.deep.include.members(expectedMoves);
+    });
+
+    it('can move diagonally', () => {
+        const queen = new Queen(Player.WHITE);
+        board.setPiece(Square.at(3, 5), queen);
+
+        const moves = queen.getAvailableMoves(board);
+
+        const expectedMoves = [
+            // Forwards diagonal
+            Square.at(4, 6), Square.at(5, 7), Square.at(4, 4), Square.at(5, 3), Square.at(6, 2), Square.at(7, 1),
+            // Backwards diagonal
+            Square.at(2, 6), Square.at(1, 7), Square.at(2, 4), Square.at(1, 3), Square.at(0, 2)
+        ];
+
+        moves.should.deep.include.members(expectedMoves);
+    });
+
+    it('cannot make any other moves', () => {
+        const queen = new Queen(Player.WHITE);
+        board.setPiece(Square.at(3, 5), queen);
+
+        const moves = queen.getAvailableMoves(board);
+
+        moves.should.have.length(25);
+    })
 });
